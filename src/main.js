5e81d013ee1d89599b6d75bfc9d56bf764730ec2
Alla10475 <sopinskaya10475@gmail.com>
2024-01-21 21:44:01 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BAZE_URL = 'https://pixabay.com/api';
-const API_KEY = '41870399-9b44301246ceb98c07efd626a';
+const modalLightboxGallery = new SimpleLightbox('.photo-container a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   photoListEl: document.querySelector('.photo-list'),
   loader: document.querySelector('.loader'),
 };
-
-refs.loader.style.display = 'none';
 
 function checkResponse(res) {
   if (!res.ok) {
@@ -21,14 +21,15 @@ function checkResponse(res) {
   return res.json();
 }
 
+refs.loader.style.display = 'none';
+
 refs.searchForm.addEventListener('submit', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
 
-
   const form = event.currentTarget;
-  const query = form.elements.query.value;
+  const query = form.elements.query.value.trim();
   refs.photoListEl.innerHTML = '';
 
   if (!query) {
@@ -37,18 +38,35 @@ function handleSearch(event) {
       position: 'topRight',
       color: 'yellow',
     });
-      
+
     return;
-    }
-    refs.loader.style.display = 'inline-block';
+  }
+  refs.loader.style.display = 'inline-block';
 
   searchPhoto(query)
-    .then(markupPhoto)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.show({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'center',
+          color: 'red',
+        });
+        return;
+      }
+      markupPhoto(data);
+    })
     .catch(err => console.log(err))
-    .finally(() => form.reset());
+    .finally(() => {
+      refs.loader.style.display = 'none';
+      form.reset();
+    });
 }
 
 function searchPhoto(value) {
+  const BASE_URL = 'https://pixabay.com/api';
+  const API_KEY = '41870399-9b44301246ceb98c07efd626a';
+
   const urlParams = new URLSearchParams({
     key: API_KEY,
     q: value,
@@ -57,49 +75,31 @@ function searchPhoto(value) {
     safesearch: true,
   });
 
-  const url = `${BAZE_URL}/?${urlParams}`;
+  const url = `${BASE_URL}/?${urlParams}`;
 
   return fetch(url).then(checkResponse);
 }
 
 function markupPhoto({ hits }) {
-  refs.loader.style.display = 'none';
-
-  if (hits.length === 0) {
-    iziToast.show({
-      title: 'Error',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      position: 'topCenter',
-      color: 'red',
-    });
-    return;
-  }
-
   const markup = hits
     .map(
-      hits => `<li class="gallery-item">
-  <a class="gallery-link" href="${hits.largeImageURL}">
+      photo => `<li class="gallery-item">
+  <a class="gallery-link" href="${photo.largeImageURL}">
     <img
       class="gallery-image"
-      src="${hits.webformatURL}"
-      data-source="${hits.imoriginal}"
-      alt="${hits.tags}"
+      src="${photo.webformatURL}"
+      alt="${photo.tags}"
     />
   </a><div class="gallery-descr">
-   <p>Likes: <br><span>${hits.likes}</span></p>
-   <p>Views: <br><span>${hits.views}</span></p>
-   <p>Comment: <br><span>${hits.comments}</span></p>
-   <p>Downloads: <br><span>${hits.downloads}</span></p></div>
+   <p>Likes: <br><span>${photo.likes}</span></p>
+   <p>Views: <br><span>${photo.views}</span></p>
+   <p>Comment: <br><span>${photo.comments}</span></p>
+   <p>Downloads: <br><span>${photo.downloads}</span></p></div>
 </li>`
     )
     .join('');
 
-  refs.photoListEl.innerHTML = markup;
+  refs.photoListEl.insertAdjacentHTML('beforeend', markup);
   modalLightboxGallery.refresh();
 }
 
-const modalLightboxGallery = new SimpleLightbox('.photo-container a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
