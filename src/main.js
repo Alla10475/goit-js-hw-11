d43561a4d5b8d28086e5b034bda6107be7d7b9d8
Alla10475 <sopinskaya10475@gmail.com>
2024-01-21 21:50:00 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -102,4 +103,3 @@ function markupPhoto({ hits }) {
   refs.photoListEl.insertAdjacentHTML('beforeend', markup);
   modalLightboxGallery.refresh();
 }
-
